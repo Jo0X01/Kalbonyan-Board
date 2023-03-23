1f271d19c8ce818e8b800470968efed73fcb2cdf
Yousef <127771585+Jo0X01@users.noreply.github.com>
2023-03-24 00:48:15 +0200

    Add files via upload

---
+++ js/Classes.js
@@ -0,0 +1,90 @@
+class Helper {
+    // Get Element by ID
+    static gid = (id, ele = document) => ele.getElementById(id);
+    // Get Elements by Class Name
+    static gcls = (className, ele = document) => ele.querySelectorAll(`.${className}`);
+    // to log in console
+    static log = (...data) => console.log(data.length == 1 ? data[0] : data);
+    // Create Element By ID and Class
+    static cele = (tag, cls, id, value = "") => {
+        tag = document.createElement(tag);
+        tag.className = cls;
+        tag.id = id;
+        if (value)
+            tag.innerHTML = value
+        return tag;
+    };
+    // Add Evenet to Element
+    static addEvent = (ele, action, handler, get_elemnt = false) => {
+        if (get_elemnt) {
+            ele = Helper.gid(ele)
+        }
+        ele.addEventListener(action, handler);
+    }
+    // Check if object is array
+    static isArray = (ele) => Array.isArray(ele);
+    // Convert object or array to iterator format
+    static toIterator = (element) => {
+        let lst = [];
+        // If input is an array, use it directly, else convert object to array
+        element = Helper.isArray(element) ? element : Object.entries(element);
+        // Loop through each key-value pair in array and add to list if not null
+        for (var i = 0; i < element.length; i++) {
+            if (element[i]) {
+                lst.push(element[i]);
+            }
+        }
+        return lst;
+    };
+    // Iterate over an object or array and call handler function on each key-value pair
+    static iter = (element, handler) => {
+        // Convert to iterator format if not already in that format
+        (Helper.isArray(element) ? element : Helper.toIterator(element)).forEach(
+            ([key, value]) => handler(key, value)
+        );
+    };
+    // Generate a unique ID string
+    static getUniqID = () =>
+        Date.now().toString(36) + Math.random().toString(36).substr(2);
+
+    // Append Children to Parent Element
+    static pushChildren = (parent, children) => {
+        try {
+            children.length;
+        } catch (e) {
+            children = [children];
+        }
+        children.forEach((child) => parent.appendChild(child));
+        return parent;
+    };
+}
+class Storage {
+    // Save a value to local storage with a given ID
+    static saveToStorage = (id, value) => {
+        window.localStorage.setItem(id, value);
+        Helper.log('sv', id); // log the save operation
+    }
+    // Get a value from local storage with a given ID
+    static getFromStorage = (id) => window.localStorage.getItem(id)
+    // Get all values from local storage as an iterator
+    static getAllStorage = () => Helper.toIterator(window.localStorage)
+    // Swap the value of an old key with a new key
+    static swapDataStorag = (oldKey, newKey) => {
+        Storage.saveToStorage(
+            newKey,
+            Storage.getFromStorage(oldKey)
+        ); // save the value with the new key
+        Storage.removeStorage(oldKey); // remove the old key
+        Helper.log('swp', oldKey, newKey); // log the swap operation
+    }
+    // Remove a value from local storage with a given ID
+    static removeStorage = (id) => {
+        window.localStorage.removeItem(id);
+        Helper.log('rm', id); // log the removal operation
+    }
+    // Remove all values from local storage
+    static removeAllStorage = () => {
+        window.localStorage.clear();
+        Helper.log("Storage Cleared"); // log the clear operation
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,163 @@
+// Start Actions Section
+edit_click = (event) => {
+    parent = event.target.parentNode.parentNode
+    parent.draggable = false
+    input = parent.firstChild
+    input.readOnly = false
+    input.focus()
+    input.select()
+}
+trsh_click = (event) => removeTask(event.target.parentNode.parentNode.id)
+inputFocusOut = (event) => {
+    e = event.target
+    parent = e.parentNode
+    e.readOnly = true
+    parent.draggable = true
+    Helper.log('edt', parent.id)
+    Storage.saveToStorage(parent.id, e.value);
+}
+// End Actions Section
+
+// Start Drag Events
+hideSeprator = (ele) => {
+    sep = ele.lastChild
+    sep.style.height = "var(--sep-hide-height)";
+    sep.style.background = "var(--sep-hide-bg)";
+}
+showSeprator = (ele) => {
+    sep = ele.lastChild
+    sep.style.height = "var(--sep-show-height)";
+    sep.style.background = "var(--sep-show-bg-color)";
+}
+dragStart = (ele) => {
+    // user start element dragged
+    // save dragged element to drag var
+    drag = ele;
+    // hide sperator if is shown
+    hideSeprator(ele)
+}
+dragEnd = (ele) => {
+    // user left the dragged element
+    drag = null; // Remove Dragged Element
+    // hide seprator
+    hideSeprator(ele);
+}
+dragOver = (event, ele) => {
+    // if dragged element come to the new element
+    // Show Seprator When Element over the parent 
+    event.preventDefault(); // Stop Browser
+    // Show Seprator
+    showSeprator(ele)
+}
+dragLeave = (event, ele) => {
+    event.preventDefault(); // Stop Browser
+    // hide seprator
+    hideSeprator(ele)
+}
+Drop = (ele) => {
+    // main function that swap element in storage and page
+    if (drag) {
+        var oldInputId = drag.id;
+        var newInputId = `${ele.id.split("-")[0]}-${oldInputId.split("-")[1]}`;
+        if (oldInputId === ele.id) return;
+        drag.id = newInputId; // change element id to the new list
+        ele.after(drag); // move element to the list
+        Storage.swapDataStorag(oldInputId, newInputId); // swap id in localStorage
+    }
+    // hide sperator
+    hideSeprator(ele);
+}
+addDragEvent = ele => {
+    Helper.addEvent(ele, 'dragstart', () => dragStart(ele));
+    Helper.addEvent(ele, 'dragend', () => dragEnd(ele));
+    Helper.addEvent(ele, 'dragover', (event) => dragOver(event, ele));
+    Helper.addEvent(ele, 'dragleave', (event) => dragLeave(event, ele));
+    Helper.addEvent(ele, 'drop', () => Drop(ele));
+    Helper.log("evnt", ele.id);
+}
+// End Drag Section
+
+// Start Tasks Section
+addTask = (id, value = "New Task", custom = false) => {
+    div = Helper.cele('div', options['inputBox'], id)
+    div.draggable = true
+    input = Helper.cele('input')
+    input.type = "text"
+    input.value = value
+    input.readOnly = true
+    actions = Helper.cele('div', options['actionBox'])
+    edt = Helper.cele('span', options['edtIcon'])
+    trsh = Helper.cele('span', options['trshIcon'])
+    Helper.addEvent(edt, 'click', edit_click)
+    Helper.addEvent(trsh, 'click', trsh_click)
+    Helper.addEvent(input, "focusout", inputFocusOut)
+    childs = [input]
+    if (custom) {
+        input.hidden = true;
+        div.className = options['custom_input']
+    } else {
+        childs.push(Helper.pushChildren(actions, [edt, trsh]))
+        Storage.saveToStorage(id, value);
+    }
+    childs.push(Helper.cele('div', options['seprator'], `sep-${id}`))
+    Helper.pushChildren(div, childs)
+    Helper.gid(ids[id.split('-')[0]]).appendChild(div)
+    Helper.log("add", div.id)
+    addDragEvent(div)
+    return div;
+}
+removeTask = (id) => {
+    Helper.gid(id).remove()
+    Storage.removeStorage(id)
+}
+// End Tasks Section
+
+// On Document Ready
+// main run first
+
+// Set Page Data
+document.title = options["title"]; // title
+Helper.gid("title").innerHTML = options["title"]; // header title
+Helper.gid("me").innerHTML = options["me"]; // developer message
+// add box lists - compeleted,not started,etc... -
+Helper.iter(lists, (id, value) => {
+    let htmlID = ids[id];
+    let child = Helper.cele(
+        "div",
+        options["headBox"],
+        htmlID.replace("-list", "")
+    );
+    Helper.pushChildren(child, [
+         // h1 header - Not Started,etc... -
+        Helper.cele("h1", "bh1", "", value),
+         // div that have element boxes
+        Helper.cele("div", options["listBox"], htmlID),
+         // add button that add new elements
+        Helper.cele("button", "", id, "+ Add"),
+    ]);
+    // append all to the main body
+    Helper.gid(options["main"]).appendChild(child);
+    addTask(`${id}-${Helper.getUniqID()}`, "", true);
+});
+// iterate click event to add all buttons
+Helper.iter(ids, (key, value) =>
+    Helper.addEvent(
+        key,
+        "click",
+        () => addTask(`${key}-${Helper.getUniqID()}`),
+        true
+    )
+);
+// update values from localStorage
+Helper.iter(
+    Storage.getAllStorage(),
+    (key, value) => {
+        try {
+            // add new Input Box
+            addTask(key, value);
+        } catch (e) {
+            // if error happen remove that input box
+            Storage.removeStorage(key);
+        }
+    }
+);
